4cc0ec7d67e8cf11a89cb220ce3a0e4282c12822
Vishal Poonia <121330381+kevinpoonia@users.noreply.github.com>
2025-09-11 03:44:09 +0530

    Create apiConfig.js

---
+++ frontend/src/apiConfig.js
@@ -0,0 +1,4 @@
+// This file centralizes the API base URL for the entire application.
+// Now, you only need to change the URL in this one place if it ever changes.
+
+export const API_BASE_URL = 'https://investmentpro-nu7s.onrender.com';
